34d65797902e3dd7cee6315c518c308ace683238
Josoa Vonjiniaina <76913187+Josoa886@users.noreply.github.com>
2023-12-22 09:39:05 +0300

    Mise a jour de l'ajout Personne

--- src/main/java/Bulletin/UI/AjoutPersonne.java
+++ src/main/java/Bulletin/UI/AjoutPersonne.java
@@ -479,7 +479,12 @@ private void Reset()
         btnImprimer.setText("Imprimer");
         btnImprimer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnImprimerActionPerformed(evt);
+                try {
+                    btnImprimerActionPerformed(evt);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -989,6 +994,6 @@ private void Reset()
     private javax.swing.JComboBox<String> moisAct;
     private javax.swing.JComboBox<String> moisnaiss;
     public javax.swing.JTextField pere;
-    private javax.swing.JTable tableCondamnation;
+    private static javax.swing.JTable tableCondamnation;
     // End of variables declaration//GEN-END:variables
 }
