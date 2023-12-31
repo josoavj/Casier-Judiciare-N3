5e2d7213def3749c2b080d52cc86070b6cbbd2e5
haritsimba <153067120+haritsimba@users.noreply.github.com>
2023-12-31 10:09:30 +0300

    dazai's latest commit

--- src/main/java/Bulletin/UI/AjoutPersonne.java
+++ src/main/java/Bulletin/UI/AjoutPersonne.java
@@ -554,7 +554,7 @@ private void Reset()
                     .addGroup(layout.createSequentialGroup()
                         .addGap(34, 34, 34)
                         .addComponent(AjoutForm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
                 .addComponent(jLabel13)
                 .addContainerGap())
         );
